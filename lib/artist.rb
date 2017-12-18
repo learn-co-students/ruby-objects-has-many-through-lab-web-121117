c01001bb9535ec594da1a2aa5a3ef0992df4af71
github username <github email address>
2017-12-18 09:52:12 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  attr_reader :songs, :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def genres
+    self.songs.map {|song| song.genre}
+  end
+end
